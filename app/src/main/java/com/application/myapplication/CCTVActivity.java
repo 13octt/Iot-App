5019ab47d76f45a2298849b3dfc8e293d8471e4c
bao duy <20521231@gm.uit.edu.vn>
2023-03-04 23:54:42 +0700

    Update bottom nav bar and xml cctv

---
+++ app/src/main/java/com/application/myapplication/CCTVActivity.java
@@ -0,0 +1,14 @@
+package com.application.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CCTVActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cctv);
+    }
+}
--- app/src/main/java/com/application/myapplication/MainActivity.java
+++ app/src/main/java/com/application/myapplication/MainActivity.java
@@ -1,14 +1,48 @@
 package com.application.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemReselectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) this);
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+
+    }
+
+    HomeFragment homeFragment = new HomeFragment();
+    CameraFragment cameraFragment = new CameraFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+
+    @Override
+    public void onNavigationItemReselected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.action_home, homeFragment).commit();
+
+            case R.id.action_camera:
+                getSupportFragmentManager().beginTransaction().replace(R.id.action_camera, cameraFragment).commit();
+
+            case R.id.action_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.action_profile, profileFragment).commit();
+        }
     }
 }
