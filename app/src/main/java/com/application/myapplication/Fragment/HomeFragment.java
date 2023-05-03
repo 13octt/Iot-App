34fad55d992cb765c114958a43b9fa21469bda91
13octt <88072761+13octt@users.noreply.github.com>
2023-05-03 11:30:24 +0700

    update xml humi and temp

--- app/src/main/java/com/application/myapplication/Fragment/HomeFragment.java
+++ app/src/main/java/com/application/myapplication/Fragment/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.application.myapplication.Fragment;
 
+import static android.service.controls.ControlsProviderService.TAG;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -7,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,29 +18,40 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.application.myapplication.R;
+import com.application.myapplication.User.SignInActivity;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class HomeFragment extends Fragment {
 
     LinearLayout roomLinearLayout;
-    private int flag = 0;
-    Button btnLamp;
+    private int flagLamp = 0, flagTV = 0, flagAC = 0, flagFan = 0, flagLight = 0;
+    Button btnLamp, btnTV, btnAC, btnFan, btnLight;
+    private MqttAndroidClient mqttAndroidClient;
+    private static final String TOPIC_TEMPERATURE = "TEMP";
+    private static final String TOPIC_HUMIDITY = "HUMI";
+    private static final String TOPIC_GAS = "GAS";
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
@@ -46,22 +60,191 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         initUI();
 
-        btnLamp.setOnClickListener(view1 -> {
-            flag++;
-            if (flag % 2 == 0) {
-                Log.d("btn", "off");
-                // Xử lí sự kiện khi tắt nút
-            } else {
-                Log.d("btn", "on");
-                // Xử lí sự kiện khi mở nút
+        String serverUri = "tcp://35.222.45.221:1883";
+        String clientId = "SMART_HOME";
+
+//        String serverUri = "mqtts://bea9028a.ala.us-east-1.emqxsl.com:8883";
+//        String clientId = "abc";
+
+        mqttAndroidClient = new MqttAndroidClient(getContext(), serverUri, clientId);
+        connectToMQTTBroker();
+        mqttAndroidClient.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.d(TAG, "Connection lost: " + cause.getMessage());
+                Toast.makeText(getContext(), "Connection lost", Toast.LENGTH_SHORT).show();
+            }
+
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                String payload = new String(message.getPayload());
+                Log.d("MQTT", "Received message on topic: " + topic + " , " + payload);
+                if (topic.equals("TEMP")) {
+                    // xử lý dữ liệu nhiệt độ
+//                    tvTemp = findViewById(R.id.temperature_value_text_view);
+//                    tvTemp.setText(payload);
+                } else if (topic.equals("HUMI")) {
+                    // xử lý dữ liệu độ ẩm
+//                    tvHumidity = findViewById(R.id.humidity_value_text_view);
+//                    tvHumidity.setText(payload);
+                } else if(topic.equals("GAS")){
+//                    tvGas = findViewById(R.id.gas_value_text_view);
+//                    tvGas.setText(payload);
+                }
+
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
 
             }
         });
+
+
+        btnLamp.setOnClickListener(view1 -> {
+            flagLamp++;
+            if (flagLamp % 2 == 0) {
+                Log.d("btn lamp", "off");
+                // Xử lí sự kiện khi tắt nút
+                publishMessage("LAMP", "0", 1);
+            } else {
+                Log.d("btn lamp", "on");
+                // Xử lí sự kiện khi mở nút
+                publishMessage("LAMP", "1", 1);
+            }
+        });
+
+        btnTV.setOnClickListener(view1 -> {
+            flagTV++;
+            if(flagTV % 2 == 0){
+                Log.d("btn tv", "off");
+                publishMessage("TV", "0", 1);
+            } else {
+                Log.d("btn tv", "on");
+                publishMessage("TV", "1", 1);
+            }
+        });
+
+        btnAC.setOnClickListener(view1 -> {
+            flagAC++;
+            if(flagAC % 2 == 0){
+                Log.d("btn AC", "off");
+                publishMessage("AC", "0", 1);
+            } else {
+                Log.d("btn AC", "on");
+                publishMessage("AC", "1", 1);
+            }
+        });
+
+        btnFan.setOnClickListener(view1 -> {
+            flagFan++;
+            if(flagFan % 2 == 0){
+                Log.d("btn FAN", "off");
+                publishMessage("FAN", "0", 1);
+            } else {
+                Log.d("btn FAN", "on");
+                publishMessage("FAN", "1", 1);
+            }
+        });
+
+        btnLight.setOnClickListener(view1 -> {
+            flagLight++;
+            if(flagLight % 2 == 0){
+                Log.d("btn LIGHT", "off");
+                publishMessage("LIGHT", "0", 1);
+            } else {
+                Log.d("btn tv", "on");
+                publishMessage("LIGHT", "1", 1);
+            }
+        });
+
 
     }
 
     private void initUI() {
         btnLamp = getView().findViewById(R.id.btn_lamp);
+        btnTV = getView().findViewById(R.id.btn_tv);
+        btnAC = getView().findViewById(R.id.btn_ac);
+        btnFan = getView().findViewById(R.id.btn_fan);
+        btnLight =getView().findViewById(R.id.btn_light);
+    }
+
+    public void connectToMQTTBroker(){
+        try {
+            String username = "thanhduy";
+            String password = "thanhduy";
+
+//            String username = "duy";
+//            String password = "duy";
+
+            MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+            mqttConnectOptions.setAutomaticReconnect(true);     //
+            mqttConnectOptions.setCleanSession(true);       //
+            mqttConnectOptions.setUserName(username);
+            mqttConnectOptions.setPassword(password.toCharArray());
+            IMqttToken token = mqttAndroidClient.connect(mqttConnectOptions);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // Connection success
+                    Log.d("MQTT", "Connect Successfully");
+//                    Toast.makeText(getContext(), "Connect Successfully", Toast.LENGTH_SHORT).show();
+
+                    subscribeToTopic(TOPIC_TEMPERATURE);
+                    subscribeToTopic(TOPIC_HUMIDITY);
+                    subscribeToTopic(TOPIC_GAS);
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Connection failed
+                    Log.d("MQTT", "Connect Failed");
+                    Toast.makeText(getContext(), "Connect Failed", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void subscribeToTopic(String topic){
+//        String topic = "SENSORS";
+        int qos = 1;
+        try {
+            IMqttToken subToken = mqttAndroidClient.subscribe(topic, qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // The message was published
+                    Log.d("MQTT", "Subscribed to topic: " + topic + ", qos: " + qos);
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.d("MQTT", "Failed to subscribe.");
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void publishMessage(String topic, String payload, int qos){
+        try {
+            MqttMessage message = new MqttMessage(payload.getBytes());
+            message.setQos(qos);
+            mqttAndroidClient.publish(topic, message);
+            Log.d("MQTT", "Topic: " + topic + ", Message: " + message);
+
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
--- app/src/main/java/com/application/myapplication/MainActivity.java
+++ app/src/main/java/com/application/myapplication/MainActivity.java
@@ -2,8 +2,10 @@ package com.application.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -17,20 +19,34 @@ import com.application.myapplication.User.SplashActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
 
-    private MqttAndroidClient mqttAndroidClient;
+//    private MqttAndroidClient mqttAndroidClient;
     private static final String TOPIC_TEMPERATURE = "TEMP";
     private static final String TOPIC_HUMIDITY = "HUMI";
     private static final String TOPIC_GAS = "GAS";
     private ImageButton logOut;
+    private MqttAndroidClient mqttAndroidClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        String serverUri = "tcp://35.222.45.221:1883";
+////        String serverUri = "mqtts://pb1f10e4.ala.us-east-1.emqxsl.com:8883";
+//
+//        String clientId = "SMART";
+//
+//        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+//        connectToMQTTBroker();
 
         // Load the store fragment by default
         replaceFragment(new HomeFragment());
@@ -64,5 +80,42 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.relative_bottom_nav, fragment);
         fragmentTransaction.commit();
+    }
+
+    public void connectToMQTTBroker(){
+        try {
+            String username = "thanhduy";
+            String password = "thanhduy";
+
+//            String username = "test";
+//            String password = "test";
+
+            MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+            mqttConnectOptions.setUserName(username);
+            mqttConnectOptions.setPassword(password.toCharArray());
+            IMqttToken token = mqttAndroidClient.connect(mqttConnectOptions);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // Connection success
+                    Log.d("MQTT", "Connect Successfully");
+//                    Toast.makeText(getApplicationContext(), "Connect Successfully", Toast.LENGTH_SHORT).show();
+
+//                    subscribeToTopic(TOPIC_TEMPERATURE);
+//                    subscribeToTopic(TOPIC_HUMIDITY);
+//                    subscribeToTopic(TOPIC_GAS);
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Connection failed
+                    Log.d("MQTT", "Connect Failed");
+//                    Toast.makeText(getApplicationContext(), "Connect Failed", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
